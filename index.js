cfa9e3713e9708078c5f62e564d432fc1fc9c776
buckeyeburl <tkgwheeler@gmail.com>
2019-08-27 23:31:42 +0100

    Added a quick post route and request example

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const http = require("http");
 const express = require("Express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
   response.send("<h1> Hellow World Woop Woop</h1>");
+});
+
+app.post("/notes", (request, response) => {
+  const note = request.body;
+  console.log(note);
+
+  response.json(note);
 });
 
 const port = 3001;
